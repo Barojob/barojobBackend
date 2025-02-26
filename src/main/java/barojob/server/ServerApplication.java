6cd8af737bc22a66a02c12ab8ac0630905274673
ZhongdanBae <128568951+ZhongdanBae@users.noreply.github.com>
2025-02-26 20:46:43 +0900

    [INIT]

---
+++ src/main/java/barojob/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package barojob.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		int a = 3;
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
