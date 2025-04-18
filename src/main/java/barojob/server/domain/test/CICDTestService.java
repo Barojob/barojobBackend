366df9a71da7aecf786b330c9188c00366431305
ZhongdanB <128568951+ZhongdanBae@users.noreply.github.com>
2025-04-18 23:17:21 +0900

    [CHORE] lombok 때문인지 확인

--- src/main/java/barojob/server/domain/test/CICDTestService.java
+++ src/main/java/barojob/server/domain/test/CICDTestService.java
@@ -3,6 +3,7 @@ package barojob.server.domain.test;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +13,9 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class CICDTestService {
-    private final StringRedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     public ResponseEntity<?> testRedis() {
