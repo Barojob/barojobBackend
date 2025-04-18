b2bdb0c81fad0b45e27928ef170dcf3ab8477aa2
ZhongdanB <128568951+ZhongdanBae@users.noreply.github.com>
2025-04-18 21:03:48 +0900

    [CHORE] elastic cache 작동 확인

--- src/main/java/barojob/server/domain/test/CICDTestService.java
+++ src/main/java/barojob/server/domain/test/CICDTestService.java
@@ -5,13 +5,15 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CICDTestService {
     StringRedisTemplate redisTemplate;
 
     public ResponseEntity<?> testRedis() {
-        redisTemplate.opsForValue().set("test", "test");
+        redisTemplate.opsForValue().set("test" + UUID.randomUUID().toString(), "test");
         return ResponseEntity.ok().build();
     }
 }
